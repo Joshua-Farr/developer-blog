179813365a0092b250876768823db7dfef9c9832
Joshua Farr <94776350+Joshua-Farr@users.noreply.github.com>
2024-03-28 21:20:09 -0500

    More styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,46 @@
+import styled from "styled-components";
+
+const StyledFooterWrapper = styled.div`
+  display: flex;
+  color: green;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  // border: 1px solid black;
+  border-radius: 20px;
+  padding-block: 2em;
+  margin-bottom: 2em;
+`;
+
+const FooterText = styled.a`
+  color: black;
+  font-size: 2.75rem;
+  margin: 0;
+
+  @media screen and (max-width: 1200px) {
+    font-size: 2rem;
+  }
+  @media screen and (max-width: 800px) {
+    font-size: 1.75rem;
+  }
+  @media screen and (max-width: 600px) {
+    font-size: 1.5rem;
+  }
+  @media screen and (max-width: 450px) {
+    font-size: 1rem;
+  }
+`;
+
+const HeaderName = styled.h3`
+  color: black;
+  font-size: 1.5rem;
+  margin: 0;
+`;
+
 export const Footer = () => {
-  return <>This is the footer</>;
+  return (
+    <StyledFooterWrapper>
+      <FooterText>Developer Blog</FooterText>
+    </StyledFooterWrapper>
+  );
 };
